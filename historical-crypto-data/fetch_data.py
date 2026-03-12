32800d080ea1e3b39b76d12cf6cba05d0fd01d8d
JanRockIT <rockrohrjan@gmail.com>
2026-03-12 11:27:20 +0000

    fetch historical data better functionality with a config file

--- historical-crypto-data/fetch_data.py
+++ historical-crypto-data/fetch_data.py
@@ -1,28 +1,24 @@
 import requests
 from datetime import datetime
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-api_key = os.getenv("COINGECKO_API_KEY")
-BASE_URL = "https://api.coingecko.com/api/v3"
-endpoint = f"{BASE_URL}/coins/bitcoin/market_chart/range"
+import time
+from config import get_headers
 
 def date_to_unix(date_string):
     dt = datetime.strptime(date_string, "%Y-%m-%d")
     return int(dt.timestamp())
-def get_headers():
-    return {
-        "accept": "application/json",
-        "x-cg-demo-api-key": api_key
+
+def get_history_range(start_date=date_to_unix("2025-6-15"), end_date=time.time(), coin="bitcoin", currency="usd"):
+    params = {
+        "vs_currency": currency,
+        "from": start_date,
+        "to": end_date
     }
-params = {
-        "vs_currency": "usd",
-        "from": date_to_unix("2025-04-3"),
-        "to": date_to_unix("2026-03-03")
-}
+    
+    response = requests.get("https://api.coingecko.com/api/v3/coins/bitcoin/market_chart/range", headers=get_headers(), params=params)
+    data = response.json()
 
-response = requests.get(endpoint, headers=get_headers(), params=params)
-data = response.json()
+    return data
 
-print(data)
+print(
+    get_history_range()
+)
